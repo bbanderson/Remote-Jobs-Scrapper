53ce82567ca1356f86f3da066357642e218b19a6
bbanderson <tombyun@naver.com>
2020-06-08 13:15:21 +0900

    Modify Port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main: app --bind 0.0.0.0:${PORT}
+web: gunicorn -b :$PORT main: app
--- main.py
+++ main.py
@@ -114,4 +114,4 @@ def export():
     return redirect("/")
 
 port = int(os.environ.get("PORT", 5000))
-app.run("0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=port)
