64db2a62ffccb64dd5f9f72cb6bbbaf44d82bb77
bbanderson <tombyun@naver.com>
2020-06-08 13:33:36 +0900

    Modify port

--- main.py
+++ main.py
@@ -113,8 +113,8 @@ def export():
   except:
     return redirect("/")
 
-# port = int(os.environ.get("PORT", 5000))
-# app.run(host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":
-  app.run()
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
