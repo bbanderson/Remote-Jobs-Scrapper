af965e36bdde91e048647f10a1347db03004e4e2
bbanderson <tombyun@naver.com>
2020-06-09 09:35:17 +0900

    Change PORT for heroku

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def export():
 
 
 
-# if __name__ == "__main__":
-#   port = int(os.environ.get("PORT", 5000))
-#   app.run(host="0.0.0.0", port=port)
-app.run("127.0.0.1")
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
+# app.run("127.0.0.1")
